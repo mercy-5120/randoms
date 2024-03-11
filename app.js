880fedabcb873738b7cfd006bddca95cf7ffb4e8
mercy-5120 <mgashau18@gmail.com>
2024-03-11 16:37:03 +0300

    used querySelector and nextElementSibling

---
+++ app.js
@@ -0,0 +1,67 @@
+
+/*var school='Moi Girls'
+console.log(`My school is ${school}`)
+let num1=90
+num1++
+var num2=30
+num2*=5
+console.log(num1, num2)
+var answer=num1+num2
+console.log(answer)
+console.log(`The sum of ${num1} and ${num2} is ${answer}`)
+console.log(firstName+'          '+secondName)*/
+/*function area(){
+    var radius=7
+    var pi=3.142
+    var answer=pi*radius*radius
+    console.log(`The area of a circle whose radius is ${radius} is ${answer}`)
+}
+area()
+function temperature(){
+    const temp=25
+    console.log(`The temperature today is ${temp} degrees celcius`)
+}
+temperature()*/
+/*let firstName='Mercy'
+let secondName='Muthoni'
+let address='Main Street'
+address='First Street'
+console.log(firstName,secondName,address)
+
+var street='Huruma'
+var country='Kenya'
+var fullMailingAddress=street+' '+ country
+console.log(fullMailingAddress)
+
+let score1=6
+let score2=15
+let score3=56
+let totalscore=score1+score2+score3
+let averagescore=totalscore/3
+console.log(totalscore,averagescore)
+
+var plates=20
+var people=7
+var remaining=plates%people
+remaining++                                                                      
+var message=`There are ${remaining} plates available`
+console.log(message);
+
+ let colors=['red','blue','black','green','orange']
+ console.log(colors[0])
+ let car={
+    colour:'black',
+    model:'porsche',
+    price:30000000
+  }
+ console.log(car.colour)*/
+ let text=document.querySelector(".one")
+ text.style.color="gold"
+text.style.background="black"
+ text.style.fontSize="25px"
+
+ let second=text.nextElementSibling
+second.style.color="pink"
+second.style.background="navy"
+second.style.fontSize="15px"
+
